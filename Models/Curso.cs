6040b2300610353a12786d2232b932d6d58cc27d
Vladimir <aizarodriguezjosevladimir@gmail.com>
2022-09-26 16:56:22 -0400

    add validadcion para curso

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -7,12 +7,14 @@ namespace aspNet.Models
     public class Curso:ObjetoEscuelaBase
     {
         [Required(ErrorMessage ="El campo nombre es requerido")]
+        [StringLength(5)] // max 5 caracteres
         public override string Nombre { set; get;}
         [Required(ErrorMessage ="Seleccione un tipo de jornada")]
         public TiposJornada? Jornada { get; set; }
         public  List <Asignatura>? Asignaturas{ get; set; }
         public List<Alumno>? Alumnos{ get; set; }
-
+        [Display(Prompt = "Dirrecion correspondencia", Name ="Addres")]
+        [MinLength(10, ErrorMessage = "La longitud minima de la direccion es 10")]
         public string? Direccion { get; set; }
 
         public string? EscuelaId { get; set; }
